6d344b6f5789edf20817a92ff0a79a35070d0c42
xing-shadow <247628073@qq.com>
2020-10-20 23:04:36 +0800

    "parse get query to struct"

--- pkg/app/Resp/Bindler.go
+++ pkg/app/Resp/Bindler.go
@@ -8,55 +8,49 @@ package Resp
 
 import (
 	"errors"
+	"net/http"
 	"reflect"
+	"strconv"
 	"strings"
+)
+
+const (
+	DefaultTag = "query"
 )
 
 type (
 	// Binder is the interface that wraps the Bind method.
 	Binder interface {
-		Bind(i interface{}, c Context) error
-	}
-
-	// DefaultBinder is the default implementation of the Binder interface.
-	DefaultBinder struct{}
-
-	// BindUnmarshaler is the interface used to wrap the UnmarshalParam method.
-	// Types that don't implement this, but do implement encoding.TextUnmarshaler
-	// will use that interface instead.
-	BindUnmarshaler interface {
-		// UnmarshalParam decodes and assigns a value from an form or query param.
-		UnmarshalParam(param string) error
+		BindParam(i interface{}) error
 	}
 )
 
 // Bind implements the `Binder#Bind` function.
-func (b *DefaultBinder) Bind(i interface{}, c Context) (err error) {
-	req := c.Request
-	params := map[string][]string{}
-	for _, param := range c.Params {
-		params[param.Key] = []string{param.Value}
+func (ctx *Context) BindParam(i interface{}) error {
+	switch ctx.Request.Method {
+	case http.MethodGet:
+		req := ctx.Request
+		var params = map[string][]string{}
+		params = req.URL.Query()
+		return ctx.bindData(i, params, DefaultTag)
+	default:
+		return ctx.BindJSON(i)
 	}
-
 }
 
-func (b *DefaultBinder) bindData(ptr interface{}, data map[string][]string, tag string) error {
+func (ctx *Context) bindData(ptr interface{}, data map[string][]string, tag string) error {
 	if ptr == nil || len(data) == 0 {
 		return nil
 	}
-
 	typ := reflect.TypeOf(ptr).Elem()
 	val := reflect.ValueOf(ptr).Elem()
-
-	//Map
 	if typ.Kind() == reflect.Map {
 		for k, v := range data {
-			val.SetMapIndex(reflect.ValueOf(k), reflect.ValueOf(v))
+			val.SetMapIndex(reflect.ValueOf(k), reflect.ValueOf(v[0]))
 		}
 		return nil
 	}
-
-	//!struct
+	// !struct
 	if typ.Kind() != reflect.Struct {
 		return errors.New("binding element must be a struct")
 	}
@@ -71,8 +65,8 @@ func (b *DefaultBinder) bindData(ptr interface{}, data map[string][]string, tag 
 		if inputFieldName == "" {
 			inputFieldName = typeField.Name
 			// If tag is nil, we inspect if the field is a struct.
-			if _, ok := structField.Addr().Interface().(BindUnmarshaler); !ok && structFieldKind == reflect.Struct {
-				if err := b.bindData(structField.Addr().Interface(), data, tag); err != nil {
+			if structFieldKind == reflect.Struct {
+				if err := ctx.bindData(structField.Addr().Interface(), data, tag); err != nil {
 					return err
 				}
 				continue
@@ -95,6 +89,91 @@ func (b *DefaultBinder) bindData(ptr interface{}, data map[string][]string, tag 
 		if !exists {
 			continue
 		}
-		// Call this first, in case we're dealing with an alias to an array type
+		if err := setWithProperType(typeField.Type.Kind(), inputValue[0], structField); err != nil {
+			return err
+		}
 	}
+	return nil
+}
+
+func setWithProperType(valueKind reflect.Kind, val string, structField reflect.Value) error {
+	switch valueKind {
+	case reflect.Ptr:
+		return setWithProperType(structField.Elem().Kind(), val, structField.Elem())
+	case reflect.Int:
+		return setIntField(val, 0, structField)
+	case reflect.Int8:
+		return setIntField(val, 8, structField)
+	case reflect.Int16:
+		return setIntField(val, 16, structField)
+	case reflect.Int32:
+		return setIntField(val, 32, structField)
+	case reflect.Int64:
+		return setIntField(val, 64, structField)
+	case reflect.Uint:
+		return setUintField(val, 0, structField)
+	case reflect.Uint8:
+		return setUintField(val, 8, structField)
+	case reflect.Uint16:
+		return setUintField(val, 16, structField)
+	case reflect.Uint32:
+		return setUintField(val, 32, structField)
+	case reflect.Uint64:
+		return setUintField(val, 64, structField)
+	case reflect.Bool:
+		return setBoolField(val, structField)
+	case reflect.Float32:
+		return setFloatField(val, 32, structField)
+	case reflect.Float64:
+		return setFloatField(val, 64, structField)
+	case reflect.String:
+		structField.SetString(val)
+	default:
+		return errors.New("unknown type")
+	}
+	return nil
+}
+
+func setIntField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0"
+	}
+	intVal, err := strconv.ParseInt(value, 10, bitSize)
+	if err == nil {
+		field.SetInt(intVal)
+	}
+	return err
+}
+
+func setUintField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0"
+	}
+	uintVal, err := strconv.ParseUint(value, 10, bitSize)
+	if err == nil {
+		field.SetUint(uintVal)
+	}
+	return err
+}
+
+func setBoolField(value string, field reflect.Value) error {
+	if value == "" {
+		value = "false"
+	}
+	boolVal, err := strconv.ParseBool(value)
+	if err == nil {
+		field.SetBool(boolVal)
+	}
+	return err
+}
+
+func setFloatField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0.0"
+	}
+	floatVal, err := strconv.ParseFloat(value, bitSize)
+	if err == nil {
+		field.SetFloat(floatVal)
+	}
+	return err
 }
