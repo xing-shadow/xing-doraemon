8afd1dbd885a9dd7d75de71e2b9f00c709d65719
wangyanlin <247628073@qq.com>
2020-12-09 16:51:41 +0800

    "修复路径问题 修复数据库连接问题"

---
+++ pkg/App/Resp/Bindler.go
@@ -0,0 +1,183 @@
+/*
+ * @Time : 2020/10/20 16:21
+ * @Author : wangyl
+ * @File : Bindler.go
+ * @Software: GoLand
+ */
+package Resp
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+const (
+	DefaultTag = "query"
+)
+
+type (
+	// Binder is the interface that wraps the Bind method.
+	Binder interface {
+		BindParam(i interface{}) error
+	}
+)
+
+// Bind implements the `Binder#Bind` function.
+func (ctx *Context) BindParam(i interface{}) error {
+	switch ctx.Request.Method {
+	case http.MethodGet:
+		req := ctx.Request
+		var params = map[string][]string{}
+		params = req.URL.Query()
+		return ctx.bindData(i, params, DefaultTag)
+	default:
+		if ctx.Binding == nil {
+			return ctx.ShouldBindJSON(i)
+		} else {
+			return ctx.BindWith(i, ctx.Binding)
+		}
+	}
+}
+
+func (ctx *Context) bindData(ptr interface{}, data map[string][]string, tag string) error {
+	if ptr == nil || len(data) == 0 {
+		return nil
+	}
+	typ := reflect.TypeOf(ptr).Elem()
+	val := reflect.ValueOf(ptr).Elem()
+	if typ.Kind() == reflect.Map {
+		for k, v := range data {
+			val.SetMapIndex(reflect.ValueOf(k), reflect.ValueOf(v[0]))
+		}
+		return nil
+	}
+	// !struct
+	if typ.Kind() != reflect.Struct {
+		return errors.New("binding element must be a struct")
+	}
+	for i := 0; i < typ.NumField(); i++ {
+		typeField := typ.Field(i)
+		structField := val.Field(i)
+		if !structField.CanSet() {
+			continue
+		}
+		structFieldKind := structField.Kind()
+		inputFieldName := typeField.Tag.Get(tag)
+		if inputFieldName == "" {
+			inputFieldName = typeField.Name
+			// If tag is nil, we inspect if the field is a struct.
+			if structFieldKind == reflect.Struct {
+				if err := ctx.bindData(structField.Addr().Interface(), data, tag); err != nil {
+					return err
+				}
+				continue
+			}
+		}
+		inputValue, exists := data[inputFieldName]
+		if !exists {
+			// Go json.Unmarshal supports case insensitive binding.  However the
+			// url params are bound case sensitive which is inconsistent.  To
+			// fix this we must check all of the map values in a
+			// case-insensitive search.
+			for k, v := range data {
+				if strings.EqualFold(k, inputFieldName) {
+					inputValue = v
+					exists = true
+					break
+				}
+			}
+		}
+		if !exists {
+			continue
+		}
+		if err := setWithProperType(typeField.Type.Kind(), inputValue[0], structField); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func setWithProperType(valueKind reflect.Kind, val string, structField reflect.Value) error {
+	switch valueKind {
+	case reflect.Ptr:
+		return setWithProperType(structField.Elem().Kind(), val, structField.Elem())
+	case reflect.Int:
+		return setIntField(val, 0, structField)
+	case reflect.Int8:
+		return setIntField(val, 8, structField)
+	case reflect.Int16:
+		return setIntField(val, 16, structField)
+	case reflect.Int32:
+		return setIntField(val, 32, structField)
+	case reflect.Int64:
+		return setIntField(val, 64, structField)
+	case reflect.Uint:
+		return setUintField(val, 0, structField)
+	case reflect.Uint8:
+		return setUintField(val, 8, structField)
+	case reflect.Uint16:
+		return setUintField(val, 16, structField)
+	case reflect.Uint32:
+		return setUintField(val, 32, structField)
+	case reflect.Uint64:
+		return setUintField(val, 64, structField)
+	case reflect.Bool:
+		return setBoolField(val, structField)
+	case reflect.Float32:
+		return setFloatField(val, 32, structField)
+	case reflect.Float64:
+		return setFloatField(val, 64, structField)
+	case reflect.String:
+		structField.SetString(val)
+	default:
+		return errors.New("unknown type")
+	}
+	return nil
+}
+
+func setIntField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0"
+	}
+	intVal, err := strconv.ParseInt(value, 10, bitSize)
+	if err == nil {
+		field.SetInt(intVal)
+	}
+	return err
+}
+
+func setUintField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0"
+	}
+	uintVal, err := strconv.ParseUint(value, 10, bitSize)
+	if err == nil {
+		field.SetUint(uintVal)
+	}
+	return err
+}
+
+func setBoolField(value string, field reflect.Value) error {
+	if value == "" {
+		value = "false"
+	}
+	boolVal, err := strconv.ParseBool(value)
+	if err == nil {
+		field.SetBool(boolVal)
+	}
+	return err
+}
+
+func setFloatField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0.0"
+	}
+	floatVal, err := strconv.ParseFloat(value, bitSize)
+	if err == nil {
+		field.SetFloat(floatVal)
+	}
+	return err
+}
